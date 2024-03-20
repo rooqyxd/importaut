92ad253d3342f98d068f1bfad17c6673f5381592
Dariusz <ddarrekk100@o2.pl>
2024-03-20 04:29:19 +0100

    src fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Navbar from "./Components/Navbar/Navbar";
-import Faq from "./Components/Faq/Faq";
+// import Faq from "./Components/Faq/Faq";
 
 function App() {
     return (
